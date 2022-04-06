94a140c67fd802f84de35cc88c3e0c5041c9658f
ArchLeaders <archparliament@gmail.com>
2022-04-06 14:15:01 -0700

    Check report mode

--- StyletWpfApp/ViewResources/Data/ToolTips.cs
+++ StyletWpfApp/ViewResources/Data/ToolTips.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StyletWpfApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,26 @@ namespace StyletWpfApp.ViewResources.Data
     {
         public static string ReportError
         {
-            get => "Reporting an error will upload system information such as file paths to a private or public GitHub Repository.\n" +
-                "Your username is hidden; however, any other names or information in file/folder paths will be uploaded.\n" +
-                "Please regard this before proceeding.\n\n" +
-                "Auto reporting will expire on 12/31/2022 or sooner to maintain security.";
+            get
+            {
+                if (ShellViewModel.UseGitHubUpload)
+                {
+                    return new(
+                        "Reporting an error will upload system information such as file paths to a private or public GitHub Repository.\n" +
+                        "Your username is hidden; however, any other names or information in file/folder paths will be uploaded.\n" +
+                        "Please regard this before proceeding.\n\n" +
+                        "Auto reporting will expire on 12/31/2022 or sooner to maintain security."
+                    );
+                }
+                else
+                {
+                    return new(
+                        "Reporting an error will upload system information such as file paths to a public Discord server.\n" +
+                        "Your username is hidden; however, any other names or information in file/folder paths will be uploaded.\n" +
+                        "Please regard this before proceeding."
+                    );
+                }
+            } 
         }
     }
 }
